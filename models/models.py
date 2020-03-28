a0ab5980b051856c03fef921deac8513fa721316
Richard Angga <richard.angga51@gmail.com>
2020-03-28 12:43:28 -0400

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -2,10 +2,23 @@
 
 from odoo import models, fields, api
 
-class ProductCategory(models.Model):
-	_inherit = 'product.category'
+class ProductTemplate(models.Model):
+	_inherit = 'product.template'
 
+	inventory_valuation = fields.Char(string="Inventory Valuation")
+	property_stock_account_input_categ_id = fields.Many2one(
+		'account.account', 'Stock Input Account', company_dependent=True,
+		domain=[('deprecated', '=', False)], oldname="property_stock_account_input_categ",
+		help="When doing real-time inventory valuation, counterpart journal items for all incoming stock moves will be posted in this account, unless "
+			 "there is a specific valuation account set on the source location. This is the default value for all products in this category. It "
+			 "can also directly be set on each product")
+	property_stock_account_output_categ_id = fields.Many2one(
+		'account.account', 'Stock Output Account', company_dependent=True,
+		domain=[('deprecated', '=', False)], oldname="property_stock_account_output_categ",
+		help="When doing real-time inventory valuation, counterpart journal items for all outgoing stock moves will be posted in this account, unless "
+			 "there is a specific valuation account set on the destination location. This is the default value for all products in this category. It "
+			 "can also directly be set on each product")
 	property_stock_valuation_account_id = fields.Many2one(
-			'account.account', 'Stock Valuation Account', company_dependent=True,
-			domain=[('deprecated', '=', False)],
-			help="When real-time inventory valuation is enabled on a product, this account will hold the current value of the products.",)
+		'account.account', 'Stock Valuation Account', company_dependent=True,
+		domain=[('deprecated', '=', False)],
+		help="When real-time inventory valuation is enabled on a product, this account will hold the current value of the products.",)
